62c87a1911a2aa6c2cfff7c35b18c0b9bbd7dca9
churhong <ok6403@gmail.com>
2018-11-24 19:28:44 +0900

    change_util file

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -24,14 +24,13 @@ def normalize_function(img):
     img = (img - np.min(img)) / (np.max(img) - np.min(img))
     img = (img - img.mean()) / math.sqrt(np.var(img))
     return img
+
 def renormalize_image(img):
     renormalized = (img + 1) * 127.5
     return renormalized
 
 def save_checkpoint(state, filename, model_dir):
-
-    # model_dir = 'drive/app/torch/save_Routing_Gate_2'
-    #model_filename = os.path.join(model_dir, filename)
+    
     model_filename = model_dir + filename
     print(model_filename)
     latest_filename = os.path.join(model_dir, 'latest.txt')
@@ -48,8 +47,6 @@ def save_checkpoint(state, filename, model_dir):
     return
 
 def load_checkpoint(model_dir):
-
-    # model_dir = 'drive/app/torch/save_Routing_Gate_2'
     latest_filename = os.path.join(model_dir, 'latest.txt')
     if os.path.exists(latest_filename):
         with open(latest_filename, 'r') as fin:
@@ -58,7 +55,6 @@ def load_checkpoint(model_dir):
         return None
     print("=> loading checkpoint '{}'".format(model_filename))
     state = torch.load(model_filename)
-
     return state
 
 def print_log(text, filename="log.txt"):
@@ -69,25 +65,24 @@ def print_log(text, filename="log.txt"):
         myfile.write(text + "\n")
 
 
+def make_one_hot() :
+    a = np.array([a for a in range(2350)])
+    b = np.zeros((2350,2350))
+    b[np.arange(2350), a] = 1
+    return b
+
 def Package_Data_Slice_Loder(number):
+    #read train data
     numpy_x = list()
     numpy_label = list()
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5))
-         ])
-    test_transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5),
-                              (0.5))
-         ])
+    numpy_onehot = list()
     with gzip.open('../Deep_model/Conpress/train_' + str(number) +'.pkl', "rb") as of:
         while True:
             try:
                 e = pickle.load(of)
                 numpy_x.extend(e[0])
                 numpy_label.extend(e[1])
-                 
+                numpy_onehot.append(make_one_hot())
                 if len(numpy_x) % 1000 == 0:
                 	print("processed %d examples" % len(numpy_x))
             except EOFError:
@@ -102,14 +97,20 @@ def Package_Data_Slice_Loder(number):
     print(X_datas.shape)
     label_datas = np.array(numpy_label)
     print(label_datas.shape)
+    onehot_datas = np.array(numpy_onehot)
+    print(onehot_datas.shape)
+    
+    #read test data
     numpy_test = list()
     numpy_label_test = list()
+    numpy_onehot_test = list()
     with gzip.open('../Deep_model/Conpress/test_' + str(number) + '.pkl', "rb") as of:
         while True:
             try:
                 e = pickle.load(of)
                 numpy_test.extend(e[0])
                 numpy_label_test.extend(e[1])
+                numpy_onehot_test.append(make_one_hot())
                 if len(numpy_test) % 1000 == 0:
                     print("processed %d examples" % len(numpy_test))
             except EOFError:
@@ -124,9 +125,74 @@ def Package_Data_Slice_Loder(number):
     print(X_test_datas.shape)
     test_label_datas = np.array(numpy_label_test)
     print(test_label_datas.shape)
+    onehot_test_datas = np.array(numpy_onehot_test)
+    print(onehot_test_datas.shape)
     train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(X_datas), torch.from_numpy(label_datas))
 
     test_dataset = torch.utils.data.TensorDataset(torch.from_numpy(X_test_datas),torch.from_numpy(test_label_datas))
+    return train_dataset, test_dataset
+
+
+def Package_Data_onehot_Slice_Loder(number):
+    # read train data
+    numpy_x = list()
+    numpy_label = list()
+    numpy_onehot = list()
+    with gzip.open('../Deep_model/Conpress/train_' + str(number) + '.pkl', "rb") as of:
+        while True:
+            try:
+                e = pickle.load(of)
+                numpy_x.extend(e[0])
+                numpy_label.extend(e[1])
+                numpy_onehot.extend(make_one_hot())
+                if len(numpy_x) % 1000 == 0:
+                    print("processed %d examples" % len(numpy_x))
+            except EOFError:
+                print('error')
+                break
+            except Exception:
+                print('error')
+                pass
+        print("unpickled total %d examples" % len(numpy_x))
+    
+    X_datas = np.array(numpy_x)
+    print(X_datas.shape)
+    label_datas = np.array(numpy_label)
+    print(label_datas.shape)
+    onehot_datas = np.array(numpy_onehot)
+    print(onehot_datas.shape)
+
+    # read test data
+    numpy_test = list()
+    numpy_label_test = list()
+    numpy_onehot_test = list()
+    with gzip.open('../Deep_model/Conpress/test_' + str(number) + '.pkl', "rb") as of:
+        while True:
+            try:
+                e = pickle.load(of)
+                numpy_test.extend(e[0])
+                numpy_label_test.extend(e[1])
+                numpy_onehot_test.extend(make_one_hot())
+                if len(numpy_test) % 1000 == 0:
+                    print("processed %d examples" % len(numpy_test))
+            except EOFError:
+                print('error')
+                break
+            except Exception:
+                print('error')
+                pass
+        print("unpickled total %d examples" % len(numpy_test))
+    
+    X_test_datas = np.array(numpy_test)
+    print(X_test_datas.shape)
+    test_label_datas = np.array(numpy_label_test)
+    print(test_label_datas.shape)
+    onehot_test_datas = np.array(numpy_onehot_test)
+    print(onehot_test_datas.shape)
+    #make train, test dataset
+    train_dataset = torch.utils.data.TensorDataset(torch.from_numpy(X_datas), torch.from_numpy(label_datas), torch.from_numpy(onehot_datas))
+    test_dataset = torch.utils.data.TensorDataset(torch.from_numpy(X_test_datas), torch.from_numpy(test_label_datas),  torch.from_numpy(onehot_test_datas))
+    
     return train_dataset, test_dataset
 
 def Package_Data_Loder():
@@ -380,4 +446,4 @@ def Slice_datalist(label_datalist, train_datalist):
 
 
 if __name__ == '__main__':
-    Package_Data_Slice_Loder(1) 
+    Package_Data_onehot_Slice_Loder(1)
