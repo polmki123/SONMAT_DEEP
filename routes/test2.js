32fdb4200764d904014dfabf9ef6aef4d231a6b8
churhong <ok6403@gmail.com>
2018-12-04 15:19:01 +0900

    Revert "Resnet3"
    This reverts commit da3165672bb7259a9d8ead62e50540aef1fc6b73.

--- routes/test2.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var data = fs.readFileSync(__dirname + '/1001my.png');
-var PNG = require('pngjs').PNG;
-var png = PNG.sync.read(data);
-
-console.log("feel not good ");
-console.log(png)
