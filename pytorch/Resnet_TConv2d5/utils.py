f379cf32da14ae96f344d6918b46919ae47be4ca
churhong <ok6403@gmail.com>
2018-12-09 20:54:07 +0900

    test

--- pytorch/Resnet_TConv2d5/utils.py
+++ pytorch/Resnet_TConv2d5/utils.py
@@ -94,11 +94,11 @@ def check_model_result_image_v2(epoch, model, number, model_dir):
         train_data = torch.utils.data.TensorDataset(torch.from_numpy(input_data))
         train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=64, shuffle=False, num_workers = 4)
         result_data = []
-        for input in train_loader :
-            input = Variable(input.cuda())
-            input = input.type(torch.cuda.FloatTensor)
-            input = normalize_image(input)
-            output = model(input)
+        for _, (data_set) in enumerate(train_loader) :
+            data_set = Variable(data_set.cuda())
+            data_set = data_set.type(torch.cuda.FloatTensor)
+            data_set = normalize_image(data_set)
+            output = model(data_set)
             output = Variable(output[1]).data.cpu().numpy()
             output =renormalize_image(output)
             result.extend(output)
